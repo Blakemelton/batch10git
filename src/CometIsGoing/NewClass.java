afc6561e1d9540402165e767c9012b946e22b415
blakemelton <blakemelton2591@gmail.com>
2021-08-11 21:24:08 -0400

    4th commit today

--- src/CometIsGoing/NewClass.java
+++ src/CometIsGoing/NewClass.java
@@ -5,7 +5,7 @@ public class NewClass {
 public static void main(String[] args) {
 	
 	
-	System.out.println("This is the first commit file");
+	System.out.println("This is the none commit file");
 	
 }
 }
